0867c39c8da85932849f97b28ab5b76c6d075f1f
Энх-Амар Болдбаатар <LPC240855@G401-CC0213.local>
2024-11-20 12:27:53 +0800

    a

--- front-end-fd/src/app/delivery-zones/page.tsx
+++ front-end-fd/src/app/delivery-zones/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import React from "react";
+import { HeaderPart } from "../_components/Header";
 
 export default function page() {
-  return <div>delivery</div>;
+  return (
+    <div>
+      <HeaderPart />
+    </div>
+  );
 }
