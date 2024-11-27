10969942fe1e06c283252fab5a89828403ebeffb
Энх-Амар Болдбаатар <LPC240855@G401-CC0213.local>
2024-11-27 11:51:53 +0800

    main bish bracnhaas pushlev

--- back-end-fd/models/food.js
+++ back-end-fd/models/food.js
@@ -1,11 +1,11 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
 
 const foodSchema = new Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   ingredient: { type: String, required: true },
   price: { type: Number, required: true },
-  categoryId: { type: String, required: true },
+  categoryId: { type: mongoose.SchemaTypes.ObjectId, required: true },
 });
 
 const foodModel = model("food", foodSchema);
