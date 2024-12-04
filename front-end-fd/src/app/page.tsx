522c0780a53d2bdb6a6163e587a060c8d9660c5d
Энх-Амар Болдбаатар <LPC240855@G401-CC0213.local>
2024-12-04 10:07:30 +0800

    small changes

--- front-end-fd/src/app/page.tsx
+++ front-end-fd/src/app/page.tsx
@@ -1,28 +1,10 @@
-"use client";
-
-import Image from "next/image";
-import { useEffect } from "react";
 import * as React from "react";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
 
 import Hero from "./_components/Hero";
 import { Footer } from "./_components/Footer";
 import { HeaderPart } from "./_components/Header";
 
 export default function Home() {
-  const fetchData = async () => {
-    try {
-      const response = await fetch("http://localhost:8888/");
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
   return (
     <div>
       <HeaderPart />
